670949f65342c62e042aa186ff57965608b3c91b
Brandon Walden <brandonshanewalden@gmail.com>
2017-09-18 06:57:46 -0400

    component and index layout with intial route

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import BaseLayout from "./components/BaseLayout"
+import Home from "./components/Home"
+import About from "./components/About"
+import Contact from './components/Contact'
+import Products from "./components/Products"
+import showProduct from "./components/showProduct"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+      <BaseLayout>
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route exact path="/" component={About}/>
+          <Route path="/contact" component={Contact} />
+          <Route exact path="/products" component={Products}/>
+          <Route path="/products/:id" component={showProduct}/>
+        </Switch>
+      </BaseLayout>
+    </BrowserRouter>
+, document.getElementById('root'));
 registerServiceWorker();
