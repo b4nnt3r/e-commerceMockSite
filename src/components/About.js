b6e9a10337c23bc2eacf1fe64e32bb0f6e57c662
Brandon Walden <brandonshanewalden@gmail.com>
2017-09-18 09:09:11 -0400

    about page setup

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class About extends Component {
+  render() {
+    return(
+      <div className="about">
+        <div>
+          <div>
+            <img id="about_img" src=''/>
+          </div>
+        </div>
+
+      </div>
+    )
+  }
+}
+export default About;
